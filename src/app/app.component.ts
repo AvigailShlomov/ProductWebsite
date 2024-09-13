5e9f6aac50096fed5ae480cea3c55353c196467e
AvigailShlomov <anm798@gmail.com>
2024-09-13 14:36:17 +0300

    implament getAllProducts method in app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,38 @@
-import { Component } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ProductDialogComponent } from './product-dialog/product-dialog.component';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(private dialog:MatDialog){}
+  constructor(private dialog: MatDialog,
+    private http: ApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllProducts()
+  }
 
   openDialog() {
     this.dialog.open(ProductDialogComponent, {
-      width:'30%'
+      width: '30%'
     });
+  }
+
+  getAllProducts() {
+    this.http.getProduct()
+      .subscribe({
+        next: (res) => {
+          console.log("product List: ", res);
+        },
+        error: (error) => {
+          alert("err whle fetching products")
+        }
+      })
   }
 }
