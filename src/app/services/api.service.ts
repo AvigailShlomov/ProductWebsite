b294e17e23671ee5efa152b90842759e7e30fa0d
AvigailShlomov <anm798@gmail.com>
2024-09-17 12:56:16 +0300

    add put and delete api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Product } from '../Modals/app-modals';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +10,21 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-postProduct(data: any) : Observable<any>{
-  return this.http.post<any>("http://localhost:3000/productList/", data);
-}
-getProduct(): Observable<any>{
-  return this.http.get<any>("http://localhost:3000/productList/")
-}
+  postProduct(data: any): Observable<any> {
+    return this.http.post<any>("http://localhost:3000/productList/", data);
+  }
 
+  getProduct(): Observable<any> {
+    return this.http.get<any>("http://localhost:3000/productList/")
+  }
+
+  putProduct(data: any, id: string): Observable<any> {
+    console.log("update procut put method: ", id);
+    return this.http.put<any>("http://localhost:3000/productList/" + id, data)
+  }
+
+  deleteProduct(id: string): Observable<any> {
+    console.log("update procut put method: ", id);
+    return this.http.delete<any>("http://localhost:3000/productList/" + id)
+  }
 }
-
-
