acb2db64d4fcdf72d776db2cdf98e39475812028
AvigailShlomov <anm798@gmail.com>
2024-09-25 22:43:57 +0300

    fix datepicker

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, { providers: [provideHttpClient(), provideAnimations()] })
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideHttpClient(),
+        provideAnimations(),
+        importProvidersFrom(MatNativeDateModule)
+    ]
+})
     .catch(err => console.error(err));
 
